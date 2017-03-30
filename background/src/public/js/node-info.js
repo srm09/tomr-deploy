3b97a17a4105894f92e1951c8896165612c0348d
Sagar Muchhal <muchhals@vmware.com>
2017-03-29 23:25:12 -0700

    Elastic addition and removal

--- background/src/public/js/node-info.js
+++ background/src/public/js/node-info.js
@@ -3,25 +3,39 @@ $(function() {
         var key = $('input[name=uploadkey]').val()
         var value = $('input[name=uploadvalue]').val()
         var url = "/upload/"+key+"/"+value
-        // a sample AJAX request
-        $.get({
-            url : url,
-            success : function(response) {
-                 alert( "Load was performed." );
-            }
-        });
+        getReq(url, (resp) => {
+            alert( "Load was performed." );
+        })
     });
 
      $('#bulk-upload-btn').click(function() {
         
         var keys = $('input[name=uploadnumber]').val()
         var url = "/upload/"+keys
-        // a sample AJAX request
-        $.get({
-            url : url,
-            success : function(response) {
-                 alert( "bulk Load was performed." );
-            }
-        });
+        getReq(url, (resp) => {
+            alert( "bulk Load was performed." );
+        })
     });
+
+    $('a[name=addRingBtn]').click(function() {
+        var ip = $(this).siblings('input[type=hidden]').val()
+        var url = "/elastic/add/"+ip
+        getReq(url, (resp) => {
+            alert('Node add request sent')
+        })
+    })
+
+    $('a[name=removeRingBtn]').click(function() {
+        var ip = $(this).siblings('input[type=hidden]').val()
+        var url = "/elastic/remove/"+ip
+        getReq(url, (resp) => {
+            alert('Node remove request sent')
+        })
+    })
 });
+var getReq = function(url, callback) {
+    $.get({
+        url : url,
+        success : callback
+    });
+}
